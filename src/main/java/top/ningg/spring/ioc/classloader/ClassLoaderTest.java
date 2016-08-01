97849ce63e22a5481f342a1233b94c7ba86e3a5a
Guo Ning <guoning.gn@gmail.com>
2016-08-01 16:28:34 +0800

    测试 class loader

--- src/main/java/top/ningg/spring/ioc/classloader/ClassLoaderTest.java
+++ src/main/java/top/ningg/spring/ioc/classloader/ClassLoaderTest.java
@@ -10,5 +10,22 @@ public class ClassLoaderTest {
         System.out.println("Current ClassLoader: " + classLoader);
         System.out.println("Parent ClassLoader: " + classLoader.getParent());
         System.out.println("GrandParent ClassLoader: " + classLoader.getParent().getParent());
+
+        // 强制装载已经存在的 class
+        Class carOne = null;
+        Class carTwo = null;
+        Class string = null;
+        try {
+            carOne = classLoader.loadClass("top.ningg.spring.ioc.Car");
+            carTwo = classLoader.loadClass("top.ningg.spring.ioc.Car");
+            System.out.println(carOne.getName() + " classLoader is: " + carOne.getClassLoader());
+
+            // JVM 类加载采取双亲委派模式, 会优先从 parent 类加载器加载类, 其中 parent 类加载器是组合方式接入
+            // 可以自定义类加载器, 破坏掉双亲委派模式.
+            string = classLoader.loadClass("java.lang.String");
+            System.out.println(string.getName() + " classLoader is: " + string.getClassLoader());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
